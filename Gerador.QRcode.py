20ce7b7a2cc51b602613d9083ac09f296247ecce
Dralker <86241975+Dralker@users.noreply.github.com>
2021-08-19 22:56:29 -0300

    Add files via upload

---
+++ Gerador.QRcode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import qrcode
+
+url = input('Digite a URL do Site aqui:', )
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=4,
+    border=5)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+
+img = qr.make_image(fill='black', back_color= 'white')
+img.show()
+
+
+
+
